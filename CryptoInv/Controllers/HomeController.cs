db9e2c39be42cb3d991af9a48d3736e035ef8a57
calpayne <34384159+calpayne@users.noreply.github.com>
2019-02-28 17:29:58 +0000

    Updated views and CSS

--- CryptoInv/Controllers/HomeController.cs
+++ CryptoInv/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CryptoInv.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CryptoInv.Controllers
 {
@@ -15,6 +16,7 @@ namespace CryptoInv.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
