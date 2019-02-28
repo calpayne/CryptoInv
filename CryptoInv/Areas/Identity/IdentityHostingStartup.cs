7eee0c002b4bf7b7073c0d69bda91e17cdca881b
calpayne <34384159+calpayne@users.noreply.github.com>
2019-02-28 15:04:28 +0000

    Initial commit

---
+++ CryptoInv/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CryptoInv.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CryptoInv.Areas.Identity.IdentityHostingStartup))]
+namespace CryptoInv.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
