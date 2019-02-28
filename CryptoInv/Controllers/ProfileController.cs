c88c7a2deb2ed7eeb0ebe09fccb1ddbdeda71332
calpayne <34384159+calpayne@users.noreply.github.com>
2019-02-28 19:33:51 +0000

    Added Profile code

---
+++ CryptoInv/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryptoInv.Data;
+using CryptoInv.Models.Profile;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoInv.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/Profile/{id}")]
+        public async Task<IActionResult> Index(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Select(u => new ProfileIndexViewModel()
+                {
+                    Id = u.Id,
+                    Username = u.UserName
+                })
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            return View(user);
+        }
+    }
+}
---
+++ CryptoInv/Models/Profile/ProfileIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoInv.Models.Profile
+{
+    public class ProfileIndexViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+    }
+}
