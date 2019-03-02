4594abe8bc55b44b7f55de74ac9100c4ddd054da
calpayne <34384159+calpayne@users.noreply.github.com>
2019-03-02 15:15:06 +0000

    Profiles now show a users investments

--- CryptoInv/Controllers/ProfileController.cs
+++ CryptoInv/Controllers/ProfileController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CryptoInv.Data;
+using CryptoInv.Data.Crypto;
+using CryptoInv.Models.Investments;
 using CryptoInv.Models.Profile;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -39,7 +41,43 @@ namespace CryptoInv.Controllers
                 return NotFound();
             }
 
-            return View(user);
+            var data = await CryptoAPI.GetDataAsync();
+
+            var investments = await _context.Investments
+                .Include(i => i.Coin)
+                .Select(i => new InvestmentViewModel()
+                {
+                    Id = i.Id,
+                    CoinId = i.CoinId,
+                    Coin = i.Coin,
+                    Amount = i.Amount,
+                    PricePerCoin = i.PricePerCoin,
+                    PricePerCoinFormatted = i.PricePerCoin.ToString("n2"),
+                    Cost = i.Cost,
+                    CostFormatted = i.Cost.ToString("n2"),
+                    InvestmentDate = i.InvestmentDate,
+                    UserId = i.UserId,
+                    PricePerCoinNow = data.DISPLAY[i.CoinId].GBP.PRICE,
+                    CostNow = Math.Round(data.RAW[i.CoinId].GBP.PRICE * i.Amount, 2),
+                    CostNowFormatted = (data.RAW[i.CoinId].GBP.PRICE * i.Amount).ToString("n2"),
+                    Profit = Math.Round((data.RAW[i.CoinId].GBP.PRICE * i.Amount) - i.Cost, 2),
+                    ProfitFormatted = ((data.RAW[i.CoinId].GBP.PRICE * i.Amount) - i.Cost).ToString("n2"),
+                    PriceChange24Hours = data.DISPLAY[i.CoinId].GBP.CHANGEPCT24HOUR
+                })
+                .Where(i => i.UserId == user.Id)
+                .ToListAsync();
+
+            ProfileIndexViewModel viewModel = new ProfileIndexViewModel()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                TotalInvested = investments.Sum(t => t.Cost).ToString("n2"),
+                TotalProfit = investments.Sum(t => t.Profit).ToString("n2"),
+                TotalAssets = investments.Sum(t => t.CostNow).ToString("n2"),
+                Investments = investments
+            };
+
+            return View(viewModel);
         }
     }
 }
--- CryptoInv/Models/Profile/ProfileIndexViewModel.cs
+++ CryptoInv/Models/Profile/ProfileIndexViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CryptoInv.Models.Investments;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,22 @@ namespace CryptoInv.Models.Profile
 {
     public class ProfileIndexViewModel
     {
+        [Display(Name = "Id"), Required]
         public string Id { get; set; }
+
+        [Display(Name = "Username"), Required]
         public string Username { get; set; }
+
+        [Display(Name = "Invested"), Required]
+        public string TotalInvested { get; set; }
+
+        [Display(Name = "Profit"), Required]
+        public string TotalProfit { get; set; }
+
+        [Display(Name = "Assets"), Required]
+        public string TotalAssets { get; set; }
+
+        [Display(Name = "Investments"), Required]
+        public List<InvestmentViewModel> Investments { get; set; }
     }
 }
