8da7ebf64b76c924fc53ef50199f38192aec9b8f
calpayne <34384159+calpayne@users.noreply.github.com>
2019-03-08 18:53:39 +0000

    Scaffolded Coins

---
+++ CryptoInv/Controllers/CoinsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CryptoInv.Data;
+
+namespace CryptoInv.Controllers
+{
+    public class CoinsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoinsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Coins
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Coins.ToListAsync());
+        }
+
+    }
+}
